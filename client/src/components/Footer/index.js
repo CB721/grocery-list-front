ea2b1d78298a16df73ff03751e2a0c8a4dfe68eb
Clint Brodar <clintbrodar@gmail.com>
2020-01-21 20:44:42 -0800

    Footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import "./style.scss";
 
 function Footer(props) {
+    const date = new Date;
+    const currentYear = date.getFullYear();
+    
     return (
         <div className="footer">
-            
+            <span>Copyright &copy; {currentYear} Clint Brodar</span>
         </div>
     )
 }
