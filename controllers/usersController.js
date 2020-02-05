5e9742fc86eac73b5fb9f8d9a69fc7bcc211536a
Clint Brodar <clintbrodar@gmail.com>
2020-02-05 09:36:29 -0800

    Password hash

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,8 +1,8 @@
 const sqlDB = require("../sql_connection");
 const { User } = require("../mongoose_models");
-const mongoose = require("mongoose");
 const checkPass = require("../validation/checkPass");
 const bcrypt = require("bcryptjs");
+const table = "uzzdv3povs4xqnxc.users";
 
 module.exports = {
     createUser: function (req, res) {
@@ -11,25 +11,75 @@ module.exports = {
         if (!checkPass(newUser.password)) {
             return res.status(400).send("Password must be at least 8 characters");
         }
-        // check if email already exists - mongo
-        User
-            .find({ email: newUser.email })
-            .then(response => {
-                if (response.length > 0) {
-                    return res.status(400).send("Email already exists");
-                }
+        // check for mongo injection
+        if (newUser.email.indexOf("$") > -1) {
+            return res.status(406).send("Invalid email");
+        } else {
+            // check if email already exists - mongo
+            User
+                .find({ email: newUser.email })
+                .then(response => {
+                    if (response.length > 0) {
+                        return res.status(400).send("Email already exists");
+                    }
+                })
+                .catch(err => console.log(err));
+        }
+        // hash user password
+        let corbato = function (resistance, id) {
+            return new Promise(function (resolve, reject) {
+                resolve(
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(resistance, salt, (err, hash) => {
+                            if (err) throw err;
+                            completeUser(hash, id);
+                        });
+                    })
+                )
             })
-            .catch(err => console.log(err));
-        // const date = new Date;
-        // create user in mongo
-        User
-            .create({ email: newUser.email })
-            .then(response => {
-                res.json(response)
-            })
-            .catch(err => res.status(422).json(err))
-        // only email is saved in mongo 
+        }
+        if (newUser.email.indexOf("$") > -1) {
+            return res.status(406).send("Invalid email");
+        } else {
+            // create user in mongo
+            // only email is saved in mongo
+            User
+                .create({ email: newUser.email })
+                .then(response => {
+                    // get id from mongo
+                    let id = response.id;
+                    // check if all user fields are complete
+                    if (newUser.first_name == undefined ||
+                        newUser.last_name == undefined ||
+                        newUser.password == undefined ||
+                        newUser.email == undefined) {
+                        return res.status(400).send("Complete all fields before continuing")
+                    } else {
+                        // password gets hashed and sent directly to sql
+                        corbato(newUser.password, id)
+                            .then()
+                            .catch(err => console.log(err));
+                    }
+                })
+                .catch(err => res.status(422).json(err));
+        }
         // transfer id, email, to sql
-        // password gets hashed and sent directly to sql
+        let completeUser = function (pass, identity) {
+            let columns = "(id, first_name, last_name, email, user_password, last_visit, joined)";
+            const first = sqlDB.escape(newUser.first_name);
+            const last = sqlDB.escape(newUser.last_name);
+            const email = sqlDB.escape(newUser.email);
+            const id = sqlDB.escape(identity);
+            const password = sqlDB.escape(pass);
+            sqlDB
+                .query(`INSERT INTO ${table} ${columns} VALUES(${id}, ${first}, ${last}, ${email}, ${password}, NOW(), NOW());`,
+                    function (err, results) {
+                        if (err) {
+                            return res.status(422).send(err);
+                        } else {
+                            return res.json(results);
+                        }
+                    });
+        }
     }
 }
