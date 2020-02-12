d01781665ab258bf4eb339f7f2f40babf0f2424e
Clint Brodar <clintbrodar@gmail.com>
2020-02-12 08:27:10 -0800

    List is draggable

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import Checkbox from "../Checkbox";
 import "./style.scss";
 
@@ -8,59 +9,118 @@ function List(props) {
     useEffect(() => {
         setList(props.list);
     }, [props.list]);
+
+    function getListItemStyle(isDragging, draggableStyle) {
+        const style = {
+            userSelect: "none",
+            background: isDragging ? "#F9FCFF" : "#3C91E6",
+            color: isDragging ? "#3C91E6" : "#F9FCFF",
+            cursor: "pointer",
+            "WebkitBoxShadow": isDragging ? "0px 3px 2px -2px rgba(47,51,56,0.64)" : "",
+            "MozBoxShadow": isDragging ? "0px 3px 2px -2px rgba(47,51,56,0.64)" : "",
+            "boxShadow": isDragging ? "0px 3px 2px -2px rgba(47,51,56,0.64)" : "",
+            ...draggableStyle
+        }
+        return style;
+    }
+    function reorder(userList, startIndex, endIndex) {
+        const result = Array.from(userList);
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+        return result;
+    }
+    function onDragEnd(result) {
+        if (!result.destination) {
+            return;
+        }
+        const items = reorder(
+            list,
+            result.source.index,
+            result.destination.index
+        )
+        setList(items);
+    }
+
     return (
-        <div className="list">
-            {list.map((item, index) => (
-                <div
-                    className="list-item"
-                    key={index}
-                >
-                    <div className="list-item-col">
-                        {item.name}
-                    </div>
-                    <div className="list-item-col">
-                        {item.store_name}
-                    </div>
-                    <div className="list-item-col">
-                        <div className="sub-col priority">
-                            {props.viewList ? (
-                                <div>
-                                    <select
-                                        className="store-dropdown"
-                                        defaultValue={item.priority}
-                                        onChange={(event) => props.changePriority(event, item.id)}
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable
+                droppableId="droppable"
+            >
+                {(provided, snapshot) => (
+                    <div
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                        className="list"
+                    >
+                        {list.map((item, index) => (
+                            <Draggable
+                                draggableId={index.toString()}
+                                index={index}
+                                key={index}
+                            >
+                                {(provided, snapshot) => (
+                                    <div
+                                        className="list-item"
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        style={getListItemStyle(
+                                            snapshot.isDragging,
+                                            provided.draggableProps.style
+                                        )}
                                     >
-                                        <option className="store-select-item" value="Low">
-                                            Low
-                                        </option>
-                                        <option className="store-select-item" value="Normal">
-                                            Normal
-                                        </option>
-                                        <option className="store-select-item" value="High">
-                                            High
-                                        </option>
-                                    </select>
-                                </div>
-                            ) : (
-                                    <div>
-                                        {item.priority}
+                                        <div className="list-item-col">
+                                            {item.name}
+                                        </div>
+                                        <div className="list-item-col">
+                                            {item.store_name}
+                                        </div>
+                                        <div className="list-item-col">
+                                            <div className="sub-col priority">
+                                                {props.viewList ? (
+                                                    <div>
+                                                        <select
+                                                            className="store-dropdown"
+                                                            defaultValue={item.priority}
+                                                            onChange={(event) => props.changePriority(event, item.id)}
+                                                        >
+                                                            <option className="store-select-item" value="Low">
+                                                                Low
+                                                            </option>
+                                                            <option className="store-select-item" value="Normal">
+                                                                Normal
+                                                            </option>
+                                                            <option className="store-select-item" value="High">
+                                                                High
+                                                            </option>
+                                                        </select>
+                                                    </div>
+                                                ) : (
+                                                        <div>
+                                                            {item.priority}
+                                                        </div>
+                                                    )}
+
+                                            </div>
+                                            <div className="sub-col">
+                                                {props.viewList ? (
+                                                    <Checkbox
+                                                        // if item has been purchased or not, change class
+                                                        class={item.purchased === 0 ? "to-get" : "done"}
+                                                        toggleClass={(event) => props.toggleClass(event, item.id, index)}
+                                                    />
+                                                ) : (<div />)}
+                                            </div>
+                                        </div>
                                     </div>
                                 )}
-
-                        </div>
-                        <div className="sub-col">
-                            {props.viewList ? (
-                                <Checkbox
-                                    // if item has been purchased or not, change class
-                                    class={item.purchased === 0 ? "to-get" : "done"}
-                                    toggleClass={(event) => props.toggleClass(event, item.id, index)}
-                                />
-                            ) : (<div />)}
-                        </div>
+                            </Draggable>
+                        ))}
+                        {provided.placeholder}
                     </div>
-                </div>
-            ))}
-        </div>
+                )}
+            </Droppable>
+        </DragDropContext>
     )
 }
 
