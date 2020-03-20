ede9b066af0a733e27a131c0a3ec8613cde55fb6
Clint Brodar <clintbrodar@gmail.com>
2020-03-20 11:59:46 -0700

    Create and get back end issue routing

--- controllers/issuesControllers.js
+++ controllers/issuesControllers.js
@@ -8,14 +8,32 @@ module.exports = {
         Issue
             .create(issue)
             .then(response => {
-                return res.status(200).json(response);
+                if (response) {
+                    return res.status(200).send("error successfully saved");
+                } else {
+                    return res.status(500).send("Issue not input into db");
+                }
             })
             .catch(err => {
                 return res.status(500).json(err);
             });
     },
     backEnd: function (req, res) {
-
+        const issue = req.body;
+        issue["front_end"] = false;
+        issue["back_end"] = true;
+        Issue
+            .create(issue)
+            .then(response => {
+                if (response) {
+                    return res.status(200).send("error successfully saved");
+                } else {
+                    return res.status(500).send("Issue not input into db");
+                }
+            })
+            .catch(err => {
+                return res.status(500).json(err);
+            });
     },
     getAll: function (req, res) {
 
@@ -48,6 +66,30 @@ module.exports = {
             });
     },
     getAllBackEnd: function (req, res) {
-
+        Issue
+            .find({ back_end: true })
+            .then(response => {
+                return res.status(200).json(response);
+            })
+            .catch(err => {
+                // save this error to db
+                // since it happened on the back end, it is back end error
+                // but the action trigger is the front end
+                Issue
+                    .create({
+                        error_code: err.code,
+                        error_message: err.name,
+                        action_trigger: "get all back end errors request",
+                        front_end: false,
+                        back_end: true
+                    })
+                    .then(errResponse => {
+                        // send back the error to the user
+                        return res.status(500).send(err)
+                    })
+                    .catch(errError => {
+                        return res.status(500).send(errError);
+                    })
+            });
     }
 }
